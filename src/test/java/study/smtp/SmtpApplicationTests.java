6bc6fae662ebb1b54425d2802c0c0b65d1fdc57a
SBSun <46879319+SBSun@users.noreply.github.com>
2023-09-28 00:23:26 +0900

    Feat: Spring Boot 프로젝트 세팅

---
+++ src/test/java/study/smtp/SmtpApplicationTests.java
@@ -0,0 +1,13 @@
+package study.smtp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmtpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
