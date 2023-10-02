7e91804b97fec8777981333b87746300f1d2c207
SBSun <46879319+SBSun@users.noreply.github.com>
2023-10-02 20:57:49 +0900

    Feat: 이메일 인증 번호 방식에서 인증 링크 방식으로 변경

--- src/main/java/study/smtp/controller/EmailController.java
+++ src/main/java/study/smtp/controller/EmailController.java
@@ -1,5 +1,6 @@
 package study.smtp.controller;
 
+import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,15 +15,22 @@ public class EmailController {
     private final EmailService emailService;
 
     @PostMapping
-    public ResponseEntity sendEmail(@RequestParam String email){
+    public ResponseEntity sendEmail(@RequestParam String email) throws MessagingException {
         emailService.sendEmail(email);
 
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    @GetMapping("/verifications")
-    public ResponseEntity<Boolean> verificationsEmail(@RequestParam String email, @RequestParam String authenticationNumber){
-        boolean isVerified = emailService.verificationsAuthenticationNumber(email, authenticationNumber);
+    @PostMapping("/authentication")
+    public ResponseEntity authenticationEmail(@RequestParam String email){
+        emailService.authenticationEmail(email);
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("/verification")
+    public ResponseEntity<Boolean> verificationEmail(@RequestParam String email){
+        boolean isVerified = emailService.verificationEmail(email);
 
         return ResponseEntity.ok(isVerified);
     }
--- src/main/java/study/smtp/service/EmailService.java
+++ src/main/java/study/smtp/service/EmailService.java
@@ -1,7 +1,10 @@
 package study.smtp.service;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.server.DelegatingServerHttpResponse;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -20,46 +23,46 @@ public class EmailService {
     private final RedisService redisService;
 
     @Transactional
-    public void sendEmail(String toEmail){
-        SimpleMailMessage emailForm = createEmailForm(toEmail);
+    public void sendEmail(String toEmail) throws MessagingException {
+        MimeMessage emailForm = createEmailForm(toEmail);
 
         emailSender.send(emailForm);
     }
 
-    private SimpleMailMessage createEmailForm(String toEmail){
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(toEmail);
-        message.setSubject("작심삼칩 인증번호");
-        message.setText(createAuthenticationNumber(toEmail));
+    private MimeMessage createEmailForm(String toEmail) throws MessagingException {
+        MimeMessage message = emailSender.createMimeMessage();
+        message.addRecipients(MimeMessage.RecipientType.TO, toEmail);
+        message.setSubject("작심삼칩 인증 메일");
+
+        String body = "";
+        body += "<h1> 안녕하세요. 작심삼칩입니다.</h1>";
+        body += "<br>";
+        body += "<p>아래 링크를 클릭하면 이메일 인증이 완료됩니다.<p>";
+        body += "<a href='http://localhost:8080/emails/authentication?email=" + toEmail + "'>인증 링크</a>";
+        
+        message.setText(body, "utf-8", "html");
+
+        redisService.setValues(toEmail, "false", authCodeExpirationMillis);
 
         return message;
     }
 
-    private String createAuthenticationNumber(String email){
-        Random random = new Random();
-        StringBuffer number = new StringBuffer();
-
-        for(int i = 0; i < 6; i++){
-            // 10 미만의 랜덤 정수
-            int randomNum = random.nextInt(10);
-            number.append(randomNum);
+    @Transactional
+    public void authenticationEmail(String email){
+        if(redisService.findByKey(email).isPresent()){
+            redisService.deleteByKey(email);
         }
 
-        redisService.setValues(email, number.toString(), authCodeExpirationMillis);
-
-        return number.toString();
+        redisService.setValues(email, "true", authCodeExpirationMillis);
     }
 
     @Transactional
-    public boolean verificationsAuthenticationNumber(String email, String authenticationNumber){
-        String findAuthenticationNumber = redisService.findByKey(email)
-                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 이메일입니다."));
-
-        if(findAuthenticationNumber.equals(authenticationNumber)){
+    public boolean verificationEmail(String email){
+        if(redisService.findByKey(email).isPresent()){
             redisService.deleteByKey(email);
-            return  true;
-        }else{
-            return false;
+            return true;
         }
+        else
+            return false;
     }
 }
